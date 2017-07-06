8d254188901d1e5d8ca1bcffc0299d5e6d89829f
javeedas <javeedemails@gmail.com>
2017-07-06 14:59:57 +0530

    Update main.js

--- js/Main.js
+++ js/Main.js
@@ -1,47 +1,52 @@
-﻿var ftmsapp = angular.module('ftms', ['ftms.controllers','ftms.Services','ngRoute']);
+var ftmsapp = angular.module('ftms', ['ftms.controllers','ftms.Services','ngRoute']);
 
 ftmsapp.config(function ($routeProvider,$locationProvider) {
     //use the HTML5 History API to remove '# tag in URL
     $locationProvider.html5Mode(true);
 
     $routeProvider.when('/', {
-        templateUrl: 'html/login.html',
+        templateUrl: 'UI/login.html',
         controller: 'loginCtrl'
     })
     .when('/Home', {
-        templateUrl: 'html/Home.html',
-        controller: 'HomeCtrl'
+        templateUrl: 'UI/Home.html',
+        controller: 'HomeCtrl',
+        isLogin:true
     })
     .when('/Home/About', {
-        templateUrl: 'html/About.html',
+        templateUrl: 'UI/about.html',
         controller: 'HomeCtrl'
 
     })
     .when('/Home/Contact', {
-        templateUrl: 'html/Contact.html',
+        templateUrl: 'UI/contact.html',
         controller: 'HomeCtrl'
 
     })
     .when('/Team/TeamDetails', {
-        templateUrl: 'html/Team.html',
-        controller: 'TeamCtrl'
+        templateUrl: 'UI/Team.html',
+        controller: 'TeamCtrl',
+        isLogin:true
 
     })
     .when('/event', {
-        templateUrl: 'html/Event.html',
-        controller : 'EventCtrl'
+        templateUrl: 'UI/Event.html',
+        controller: 'EventCtrl',
+        isLogin:true
 
     })
-    .when('/Account/AddUser', {
-        templateUrl: 'html/adduser.html',
-        controller : 'loginCtrl'
+    .when('/Home/Account/addUser', {
+        templateUrl: 'UI/adduser.html',
+        controller: 'loginCtrl',
+        isLogin:true
     })
     .when('/Funds', {
-        templateUrl: 'html/funds.html',
-        controller: 'FundCtrl'
+        templateUrl: 'UI/funds.html',
+        controller: 'FundCtrl',
+        isLogin:true
     })
     .otherwise({
-        redirectTo: '/'
+        redirectTo: '/Home'
     });
 
 });
